58e1a541962384e00c423dfa0caf3a0088a3f226
CommanderStrax <tjgerot@cox.net>
2015-10-07 00:03:03 -0500

    Added console logs

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 function brightenup() {
+    console.log("Starting");
     var picture = document.getElementsByClassName("mwlogo")[0];
     picture.setAttribute('src',"images/MW-hover.png");
+    console.log("Ending");
 }
