517ba9b0022b08b7065a7013106174eeda7b8dea
CommanderStrax <tjgerot@cox.net>
2015-10-07 18:39:06 -0500

    Removed the unnecessary custom.js file

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-function brightenup() {
-    console.log("Starting");
-    var picture = document.getElementsByClassName("mwlogo")[0];
-    picture.setAttribute('src',"images/MW-hover.png");
-    console.log("Ending");
-}
