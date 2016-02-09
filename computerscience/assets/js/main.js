dfbe3fdf8c00a3bae318f927987ff790eda69cdf
Thomas Gerot <tjgerot@cox.net>
2016-02-08 21:26:38 -0600

    Some of the best formatting you've seen

--- computerscience/assets/js/main.js
+++ computerscience/assets/js/main.js
@@ -1,9 +1,11 @@
-/*
-	Twenty by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
+function gc(b, i) {
+    if (b === "t") {
+        document.getElementById("c" + i).style.color = "green";
+    }
+    else {
+        document.getElementById("c" + i).style.color = "";
+    }
+}
 (function($) {
 
 	skel.breakpoints({
--- computerscience/script.js
+++
@@ -1,8 +0,0 @@
-function gc(b, i) {
-    if (b === "t") {
-        document.getElementById("c" + i).style.color = "green";
-    }
-    else {
-        document.getElementById("c" + i).style.color = "";
-    }
-}
